0012fb80fa55e654c1b369b431a42ef6a2ce8b51
ANKIT KUMAR <ankit@dsvcorp.com.au>
2026-07-13 13:19:46 +0530

    refactor: remove aws-kms-encryption project

--- aws-kms-encryption/backend/src/main/java/com/encryption/backend/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.encryption.backend.repository;
-
-import com.encryption.backend.model.Student;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends MongoRepository<Student, String> {
-}
